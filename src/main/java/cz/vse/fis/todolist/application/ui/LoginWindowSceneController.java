473fb59c0044ecd178ffddcc4bd082e786211a7c
Michal Cúth <cuthmichal@gmail.com>
2021-01-22 10:49:33 +0100

    added LoginWindowSceneController with JavaFX elements for login credentials

---
+++ src/main/java/cz/vse/fis/todolist/application/ui/LoginWindowSceneController.java
@@ -0,0 +1,15 @@
+package cz.vse.fis.todolist.application.ui;
+
+import javafx.scene.control.TextField;
+
+/**
+ * LoginWindowSceneController class contains methods to check user's login credentials. After user provides valid
+ * username and password he is redirected to the main window of application.
+ *
+ * @version 1.0.0
+ */
+public class LoginWindowSceneController {
+    //scene elements
+    public TextField usernameTextField;
+    public TextField passwordTextField;
+}
