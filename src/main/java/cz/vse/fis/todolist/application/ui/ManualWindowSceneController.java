9d4cccdf66ee42a1369cf5e3afb74a291890621d
Michal Cúth <cuthmichal@gmail.com>
2021-01-22 11:43:15 +0100

    added ManualWindowSceneController

---
+++ src/main/java/cz/vse/fis/todolist/application/ui/ManualWindowSceneController.java
@@ -0,0 +1,16 @@
+package cz.vse.fis.todolist.application.ui;
+
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
+/**
+ * ManualWindowSceneController class contains methods to handle events in Manual window scene, e.g. show
+ * text to manual's chapter chosen by user
+ *
+ * @version 1.0.0
+ */
+public class ManualWindowSceneController {
+    //scene elements
+    public ListView manualMenuListView;
+    public Text chapterText;
+}
