d103851e71a2e65ca2f3d67ece1222bd670ee9e0
Michal Cúth <cuthmichal@gmail.com>
2021-01-22 14:16:52 +0100

    added SettingsWindowSceneController

---
+++ src/main/java/cz/vse/fis/todolist/application/ui/SettingsWindowSceneController.java
@@ -0,0 +1,16 @@
+package cz.vse.fis.todolist.application.ui;
+
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+/**
+ * SettingsWindowSceneController class contains methods to change user password, password hint, avatar or to delete
+ * user account
+ *
+ * @version 1.0.0
+ */
+public class SettingsWindowSceneController {
+    //scene elements
+    public ListView settingsMenuListView;
+    public VBox settingsContent;
+}
