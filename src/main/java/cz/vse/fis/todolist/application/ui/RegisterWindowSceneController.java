5fe655c34b47b84cfc908c6759176650831a4db9
Michal Cúth <cuthmichal@gmail.com>
2021-01-22 11:00:18 +0100

    added RegisterWindowSceneController with JavaFX elements for creating new account

---
+++ src/main/java/cz/vse/fis/todolist/application/ui/RegisterWindowSceneController.java
@@ -0,0 +1,16 @@
+package cz.vse.fis.todolist.application.ui;
+
+import javafx.scene.control.TextField;
+
+/**
+ * RegisterWindowSceneController class contains methods to create new user account
+ *
+ * @version 1.0.0
+ */
+public class RegisterWindowSceneController {
+    //scene elements
+    public TextField usernameTextField;
+    public TextField passwordTextField;
+    public TextField confirmPasswordTextField;
+    public TextField passwordHintTextField;
+}
