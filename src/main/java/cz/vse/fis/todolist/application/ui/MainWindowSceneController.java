fdc952572cdaf3e0f50e9ea7ff7c8bcbe76922fe
Michal Cúth <cuthmichal@gmail.com>
2021-01-24 12:28:49 +0100

    added methods to MainWindowSceneController for switching between scenes

--- src/main/java/cz/vse/fis/todolist/application/ui/MainWindowSceneController.java
+++ src/main/java/cz/vse/fis/todolist/application/ui/MainWindowSceneController.java
@@ -56,6 +56,7 @@ public class MainWindowSceneController {
      * @param actionEvent
      */
     public void showManualWindow(ActionEvent actionEvent) {
+        App.activateScene("manual");
     }
 
     /**
@@ -64,6 +65,7 @@ public class MainWindowSceneController {
      * @param actionEvent
      */
     public void showSettingsWindow(ActionEvent actionEvent) {
+        App.activateScene("settings");
     }
 
     /**
